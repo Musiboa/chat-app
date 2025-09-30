da56a9d31263e7ec2d3e07459df0206d71b061a7
tangxi <tangxi@readboy.com>
2025-09-30 17:25:44 +0800

    feat: 登录初步

--- chat-app-client/src/views/login/index.vue
+++ chat-app-client/src/views/login/index.vue
@@ -4,17 +4,19 @@
       <h1 class="form-title">登录</h1>
       <el-form :model="userInfo" class="login-form">
         <el-form-item prop="username">
-          <el-input v-model="userInfo.username" placeholder="请输入用户名" prefix-icon="User" clearable></el-input>
+          <el-input v-model="userInfo.username" placeholder="请输入用户名" prefix-icon="User" clearable
+            auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item prop="password">
           <el-input v-model="userInfo.password" type="password" show-password placeholder="请输入密码" prefix-icon="Lock"
-            clearable></el-input>
+            clearable auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item prop="phone" v-show="isLogup">
           <el-input v-model="userInfo.phone" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" class="confirm-btn" @click="isLogup ? logup() : login()">{{ isLogup ? '注册' : '登录' }}</el-button>
+          <el-button type="primary" class="confirm-btn" @click="isLogup ? logup() : login()">{{ isLogup ? '注册' : '登录'
+            }}</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -23,7 +25,9 @@
 <script setup>
 import axios from 'axios';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
+const $router = useRouter();
 let isLogup = ref(false);
 const userInfo = ref({
   username: '',
@@ -43,7 +47,10 @@ const logup = () => {
 }
 const login = () => {
   axios.post('http://localhost:3000/api/auth/login', userInfo.value).then(res => {
+    const { data } = res;
+    localStorage.setItem('token', data.token);
     console.log(res)
+    $router.push({ name: 'chat' });
   }).catch(err => {
     console.log(err)
   })
@@ -61,21 +68,32 @@ const login = () => {
     box-sizing: border-box;
     width: 400px;
     height: 50vh;
+    display: flex;
+    flex-direction: column;
     box-shadow: 0px 0px 12px rgba(0, 0, 0, .12);;
     border-radius: 2vh;
-    padding: 20px;
+    padding: 50px;
     background-color: #fff;
   }
   .form-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30%;
     font-size: 24px;
     font-weight: 500;
     text-align: center;
-    margin-bottom: 20px;
   }
-  .login-form { 
+  .login-form {
+    flex: 1; 
     width: 100%;
     height: 100%;
-    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .el-input {
+      height: 40px;
+    }
     .confirm-btn {
       width: 100%;
     }

fc9dd381811540634a1e710df766818055c2ebee
tangxi <tangxi@readboy.com>
2025-10-11 16:58:28 +0800

    feat: 增加退出登录交互

--- chat-app-client/src/views/chat/index.vue
+++ chat-app-client/src/views/chat/index.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="chat-container">
-    <ul class="side-menu">
-      <li class="menu-item">
-        <el-avatar shape="square" :size="40" :src="userInfo.avatar"></el-avatar>
-      </li>
-      <li v-for="route in menuRoutes" :key="route.name" class="menu-item" @click="navigateTo(route.path)">
-        <el-icon :size="24">
-          <component :is="route.meta.icon"></component>
-        </el-icon>
-        <span>{{ route.meta.title }}</span>
-      </li>
-    </ul>
+    <div class="side-menu">
+      <ul class="menu-list">
+        <li class="menu-item">
+          <el-avatar shape="square" :size="40" :src="userInfo.avatar"></el-avatar>
+        </li>
+        <li v-for="route in menuRoutes" :key="route.name" class="menu-item" @click="navigateTo(route.path)">
+          <el-icon :size="24">
+            <component :is="route.meta.icon"></component>
+          </el-icon>
+          <span>{{ route.meta.title }}</span>
+        </li>
+      </ul>
+      <ul class="action-list">
+        <el-popover trigger="click" placement="right" popper-class="action-popover" popper-style="padding: 5px;">
+          <template #reference>
+            <li class="action-item">
+              <el-icon :size="24">
+                <MoreFilled />
+              </el-icon>
+            </li>
+          </template>
+          <ul class="popover-list">
+            <li class="popover-item" @click="handleLogout">
+              <el-icon :size="16">
+                <SwitchButton />
+              </el-icon>
+              <span>退出登录</span>
+            </li>
+          </ul>
+        </el-popover>
+      </ul>
+    </div>
     <div class="main-container">
       <router-view></router-view>
     </div>
@@ -20,7 +41,7 @@
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { menuRoutes } from '@/router';
-import { getCurrentUser } from '@/api/api';
+import { getCurrentUser, reqLogout } from '@/api/api';
 const $router = useRouter();
 const navigateTo = (path) => {
   $router.push(path);
@@ -32,6 +53,15 @@ const getUserInfo = async () => {
     userInfo.value = data;
   } catch (error) {
     console.error('获取当前用户信息失败:', error);
+  }
+};
+const handleLogout = async () => {
+  try {
+    await reqLogout();
+    localStorage.removeItem('token');
+    $router.push({ name: 'login' });
+  } catch (error) {
+    console.error('退出登录失败:', error);
   }
 };
 onMounted(() => {
@@ -49,6 +79,9 @@ onMounted(() => {
   box-sizing: border-box;
   padding: 20px 10px;
   border-right: 1px solid #dfe1e2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   .menu-item {
     display: flex;
     flex-direction: column;
@@ -59,6 +92,26 @@ onMounted(() => {
     border-radius: 8px;
     &:not(:first-child):hover {
       background-color: #f5f7f9;
+    }
+  }
+  .action-list {
+    .action-item {
+      display: flex;
+      justify-content: center;
+    }
+  }
+}
+.action-popover {
+  .popover-list {
+    .popover-item {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      padding: 10px;
+      cursor: pointer;
+      &:hover {
+        background-color: #f5f7f9;
+      }
     }
   }
 }

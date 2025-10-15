13d29e1a80c53437607396e29eacba3db50575c0
tangxi <tangxi@readboy.com>
2025-10-15 16:08:19 +0800

    feat: 存储用户信息

--- chat-app-client/src/stores/user.js
+++ chat-app-client/src/stores/user.js
@@ -2,9 +2,10 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
-  const user = ref({})
+  const user = ref(JSON.parse(localStorage.getItem('user') || '{}'))
   const setUser = (data) => {
     user.value = data
+    localStorage.setItem('user', JSON.stringify(data))
   }
 
   return { user , setUser }
